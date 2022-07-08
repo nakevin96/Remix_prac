5238cc577fb154a90be89bfab469ff48b20eb90b
"Yoon" <"nakevin96@hanyang.ac.kr">
2022-07-08 18:14:50 +0900

    inheritance and override

--- contracts/2_importing_contracts_into_others/ExtraStroage.sol
+++ contracts/2_importing_contracts_into_others/ExtraStroage.sol
@@ -1,10 +1,18 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.8;
+pragma solidity ^0.8.0;
 
 import "./SimpleStorage.sol";
 
 contract ExtraStorage is SimpleStorage{
+    /*
+    만약 contract를 상속받아 사용하고 싶다면
+    우리는 두 가지 키워드에 대해 알아야 한다.
+    바로 override와 virtual이다.
+    
+    override를 하고자 하는 함수를 작성할 때는 override 키워드를 붙여주어야 한다.
+    그리고 기존에 존재하는 함수는 override를 하기위해서 virtual 키워드를 가져야 한다.
+    */
     function store(uint256 _favoriteNumber) public virtual override {
         favoriteNumber = _favoriteNumber + 5;
     }
--- contracts/2_importing_contracts_into_others/StorageFactory.sol
+++ contracts/2_importing_contracts_into_others/StorageFactory.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+/*
+import문을 사용하면 다른 contract를 사용할 수 있게 된다.
+주의해야 할 점은 solidity version이 호환가능해야 한다는 점이다.
+*/
 import "./SimpleStorage.sol";
 
 contract StorageFactory{
@@ -11,8 +15,14 @@ contract StorageFactory{
         simpleStorageArray.push(simpleStorage);
     }
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public{
-        //Address
-        //ABI
+        /*
+        어떤 contract와 상호작용(interact)하기 위해서는
+        Contract의 address와 ABI가 필요하다.
+
+        ABI는 Application Binary Interface의 약자로,
+        ABI는 어떻게 해당 contract와 상호작용을 할 수 있는지에 관한 정보가 담겨있다.
+        Contract를 통해 할 수 있는 모든 작업에 대한 input과 output에 대한 정보를 알려준다.
+        */
         //SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
         simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
     }
