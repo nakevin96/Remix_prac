d82c96d8e5f4bbd1d158ddf728932d55b9559b2d
"Yoon" <"nakevin96@hanyang.ac.kr">
2022-07-09 22:35:05 +0900

    payable키워드

--- contracts/3_remix_fund_me/FundMe.sol
+++ contracts/3_remix_fund_me/FundMe.sol
@@ -9,5 +9,32 @@ USD기준으로 최소 funding value를 지정할 것이다.
 */
 
 contract FundMe {
-    
+    unit256 public minimumUsd = 50;
+    /*
+    이더리움 플랫폼 위에서 이더 코인을 전송하는 스마트 컨트랙트를 작성하기 위해서는 반드시 payable키워드가 있어야 한다.
+    즉 payable키워드가 들어간 function만이 이더를 주고 받을 수 있다는 의미이다.
+    이 때 payable이 address와 함께 사용되면 지불 가능한 주소 : address payable이 되고
+    function과 사용되면 지불가능한 함수 function payable이 된다.
+    */
+    function fund() public payable{
+        /*
+        이 부분에 funding을 받을 코드를 작성할 것이다.
+        이 때 우리는 어떻게 ETH을 이 contract에서 받을 것인지 생각해봐야 한다.
+        Transaction이 발생할 때 transaction은 다음과 같은 field를 갖는다.
+        - Nonce: 계정의 transaction count
+        - Gas Price: unit of gas의 가격을 의미하며 단위는 wei이다.
+        - Gas Limit: transaction이 사용할 수 있는 최대 가스 값을 뜻한다.
+        - To: transaction이 발생했을 때 value를 받는 주소
+        - Value: 얼마만큼 보낼 것인지, 단위는 wei
+        - Data: To address에 어떤 걸 보내는지
+        - v, r, s: transaction signatur의 구성요소
+        */
+        // 1e18은 1 * 10 ** 18과 같다. 1*10**18 wei 가 1 eth이기 때문에 require과 합쳐
+        // 1eth이상을 제한으로 걸 수 있다.
+        // require는 체크 함수로 앞에서 조건을 체크하고 만약 만족하지 않는다면 두번째 인자를 바탕으로 에러를 반환한다.
+        //require(msg.value >= 1e18, "Didn't send enough!");
+        require(msg.value >= minimumUsd, "Didn't send enough!");
+    }
+
+    //function withdraw(){}
 }
