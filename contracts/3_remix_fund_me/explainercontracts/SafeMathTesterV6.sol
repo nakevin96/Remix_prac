25260e4e7199f70659b6a7ae80d4b760b442010a
"Yoon" <"nakevin96@hanyang.ac.kr">
2022-07-10 16:26:18 +0900

    checked, unchecked

--- contracts/3_remix_fund_me/explainercontracts/SafeMathTesterV6.sol
+++ contracts/3_remix_fund_me/explainercontracts/SafeMathTesterV6.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.6.0;
 
 contract SafeMathTester{
+    // unchecked라는 것은 최대 수를 초과하면 가장 작은 수로 돌아감을 의미한다.
+    // 즉 255가 저장된 상태에서 add()함수를 호출해 bigNumber에 1이 더해지면 0이된다는 뜻이다.
     uint8 public bigNumber = 255; // unchecked
 
     function add() public{
--- contracts/3_remix_fund_me/explainercontracts/SafeMathTesterV8.sol
+++ contracts/3_remix_fund_me/explainercontracts/SafeMathTesterV8.sol
@@ -2,9 +2,15 @@
 pragma solidity ^0.8.0;
 
 contract SafeMathTester{
-    uint8 public bigNumber = 255; //checked
+    /*
+    버전 8부터는 unchecked로 'overflow'나 'underflow'가 발생했는지 아닌지 체크하여
+    만약 발생하면 에러를 반환한다.
+    */
+    uint8 public bigNumber = 255; // checked
 
     function add() public{
-        unchecked {bigNumber = bigNumber +1;}
+        // 만약 overflow가 발생했을 때 에러를 발생시키는 것이 아닌 다시 0으로, 최소로 돌아가게 하고 싶다면 변수에 checked를 주석으로 붙이고
+        // unchecked키워드로 묶어주면 된다.
+        unchecked{bigNumber = bigNumber +1;}
     }
 }
